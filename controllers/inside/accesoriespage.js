43a5adfd03e7ab74066dfd6bfa296800dc2cdec3
sanjayperingayil2000 <109661238+sanjayperingayil2000@users.noreply.github.com>
2023-02-10 12:36:54 +0530

    Update accesoriespage.js

--- controllers/inside/accesoriespage.js
+++ controllers/inside/accesoriespage.js
@@ -3,7 +3,7 @@ const {findAllFromDb} = require("../connection")
 const accesoriespageData=async(req,res)=>{
     try {
         const data = await findAllFromDb("accesoriespageData");
-        return res.status(200).send({accesoriespageData :data});
+        return res.status(200).send(data);
     } catch (error) {
         console.log("error occured while fetching",error);
         return res.status(500).send({message:"something went wrong"});
