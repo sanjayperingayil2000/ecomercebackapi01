f2523b0e7f0340ab5fe935e44aa5166f22796ed2
sanjayperingayil2000 <109661238+sanjayperingayil2000@users.noreply.github.com>
2023-02-10 12:37:37 +0530

    Update ipadpage.js

--- controllers/inside/ipadpage.js
+++ controllers/inside/ipadpage.js
@@ -3,7 +3,7 @@ const {findAllFromDb} = require("../connection")
 const ipadpageData=async(req,res)=>{
     try {
         const data = await findAllFromDb("ipadpageData");
-        return res.status(200).send({ipadpageData :data});
+        return res.status(200).send(data);
     } catch (error) {
         console.log("error occured while fetching",error);
         return res.status(500).send({message:"something went wrong"});
