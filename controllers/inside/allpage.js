b835b3cf17aa5b8303a3f9361f13ac6ab02e3ef0
sanjayperingayil2000 <109661238+sanjayperingayil2000@users.noreply.github.com>
2023-02-10 12:37:17 +0530

    Update allpage.js

--- controllers/inside/allpage.js
+++ controllers/inside/allpage.js
@@ -3,7 +3,7 @@ const {findAllFromDb} = require("../connection")
 const allpageData=async(req,res)=>{
     try {
         const data = await findAllFromDb("allpageData");
-        return res.status(200).send({allpageData :data});
+        return res.status(200).send(data);
     } catch (error) {
         console.log("error occured while fetching",error);
         return res.status(500).send({message:"something went wrong"});
