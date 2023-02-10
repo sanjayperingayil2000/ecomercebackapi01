09d8cf49aad25b0924fa0bddbe771cc26488c909
sanjayperingayil2000 <109661238+sanjayperingayil2000@users.noreply.github.com>
2023-02-10 12:33:46 +0530

    Update accesories.js

--- controllers/accesories.js
+++ controllers/accesories.js
@@ -3,7 +3,7 @@ const {findAllFromDb} = require("./connection")
 const accesoriesMainPic=async(req,res)=>{
     try {
         const data = await findAllFromDb("accesoriesMainPic");
-        return res.status(200).send({accesoriesMainPic :data});
+        return res.status(200).send(data);
     } catch (error) {
         console.log("error occured while fetching",error);
         return res.status(500).send({message:"something went wrong"});
@@ -13,7 +13,7 @@ const accesoriesMainPic=async(req,res)=>{
 const accesoriesData=async(req,res)=>{
     try {
         const data = await findAllFromDb("accesoriesData");
-        return res.status(200).send({accesoriesData :data});
+        return res.status(200).send(data);
     } catch (error) {
         console.log("error occured while fetching",error);
         return res.status(500).send({message:"something went wrong"});
